b96aae311099f63406a93c9b69e23614afe13ce0
Jaeden666 <j.barba1@share.epsb.ca>
2025-04-16 14:43:43 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
